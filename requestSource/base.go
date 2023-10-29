34c6a0dab30b3ec71ab8d1c562650c9ac56f5cf4
Kamil Szymczak <kamil.szymczak@hotmail.com>
2023-10-29 19:02:37 +0000

    interface for accessing response payload and team names info hydration

--- requestSource/base.go
+++ requestSource/base.go
@@ -5,8 +5,12 @@ type Payload interface {
 	LivescoreData | DummyData
 }
 
-type RequestAccessor[T Payload] interface {
+type Sourceable[T Payload] interface {
 	GetData() T
+	SetData(T) T
+}
+
+type ResponseAccessor interface {
 	GetTeam1Name() string
 	GetTeam2Name() string
 	GetTeam1Score() int
--- requestSource/livescore.go
+++ requestSource/livescore.go
@@ -1,21 +1,39 @@
 package requestSource
 
-// type LivescoreAccessor interface {
-// 	RequestAccessor[LivescoreData]
-// 	GetTeam1Name() string
-// 	GetTeam2Name() string
-// 	GetTeam1Score() int
-// 	GetTeam2Score() int
-// }
+import "strconv"
 
 type LivescoreData struct {
-	EventID string `json:"Eid"`
-	EventStatus string `json:"Eps"`
-	Team1ScoreFT string `json:"Tr1OR"`
-	Team2ScoreFT string `json:"Tr2OR"`
-	EventFinished int `json:"epr"` //epr=0 game not started, epr=1 game ongoing, epr=2 game finished
+	EventID       string   `json:"Eid"`
+	EventStatus   string   `json:"Eps"`
+	Team1Info     teamInfo `json:"T1"`
+	Team2Info     teamInfo `json:"T2"`
+	Team1ScoreFT  string   `json:"Tr1OR"`
+	Team2ScoreFT  string   `json:"Tr2OR"`
+	EventFinished int      `json:"epr"` //epr=0 game not started, epr=1 game ongoing, epr=2 game finished
 }
 
-func (p *LivescoreData) GetData() *LivescoreData {
-	return p
+type teamInfo []struct {
+	TeamName string `json:"Nm"`
+}
+
+func (p *LivescoreData) GetTeam1Name() string {
+	return p.Team1Info[0].TeamName
+}
+
+func (p *LivescoreData) GetTeam2Name() string {
+	return p.Team2Info[0].TeamName
+}
+
+func (p *LivescoreData) GetTeamHomeScore() int {
+	n, _ := strconv.Atoi(p.Team1ScoreFT)
+	return n
+}
+
+func (p *LivescoreData) GetTeamAwayScore() int {
+	n, _ := strconv.Atoi(p.Team2ScoreFT)
+	return n
+}
+
+func (p *LivescoreData) GetData() LivescoreData {
+	return *p
 }
